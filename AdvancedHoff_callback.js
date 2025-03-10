3666e1f0394eb13c4b679177dd15b23341188874
Dharamsen <dharamsen1612@gmail.com>
2025-03-10 10:51:03 +0530

    .

--- AdvancedHoff_callback.js
+++ AdvancedHoff_callback.js
@@ -1,64 +1,64 @@
 // Create a function that takes a callback and executes it after every `n` seconds indefinitely.
-// function abc(fn, time)
-// {
-//     setInterval(fn,time)
+function abc(fn, time)
+{
+    setInterval(fn,time)
 
-// }
+}
 
-// abc(function()
-// {
-//     console.log("hello");
+abc(function()
+{
+    console.log("hello");
 
-// },2000)
+},2000)
 
 //--------------------------------------------------------------------------------------------
 
 // Implement a function that returns a function with a preset greeting (Closure).
 
-// function greet(value)
-// {
-//     return function(name)
-//     {
-//         console.log(`${value} ${name}`);
+function greet(value)
+{
+    return function(name)
+    {
+        console.log(`${value} ${name}`);
 
-//     }
-// }
+    }
+}
 
-// let ans = greet("Hellooo")
-// ans("john")
-// ans("virat")
+let ans = greet("Hellooo")
+ans("john")
+ans("virat")
 
-// let spanish = greet("Holaa!!")
-// spanish("Pablooo")
+let spanish = greet("Holaa!!")
+spanish("Pablooo")
 
 // -----------------------------------------------------------------------------------------------------------------------------------
 
 // Implement a function that takes a callback and only executes it once (HOF + Closure).
 
-//  function caller(fn)
-//  {
-//     let executed = false;
-//     return function()
-//     {
-//         if(!executed)
-//         {
-//             executed = true;
-//             fn();
+ function caller(fn)
+ {
+    let executed = false;
+    return function()
+    {
+        if(!executed)
+        {
+            executed = true;
+            fn();
 
-//         }
+        }
 
-//     }
+    }
 
-//  }
+ }
 
-// let ans = caller(function()
-// {
-//     console.log("callback should only called once ");
+let ans = caller(function()
+{
+    console.log("callback should only called once ");
 
-// })
-// ans();
-// ans();
-// ans();
+})
+ans();
+ans();
+ans();
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------
 
