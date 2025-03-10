6c70e02e19c0d2aead6ebed49c9b858082f4246b
Dharamsen <dharamsen1612@gmail.com>
2025-03-10 09:20:57 +0530

    questionsoncallback

--- Callback_closures.js
+++ Callback_closures.js
@@ -1,60 +1,60 @@
 // Create a function that takes another function as an argument and calls it after 3 seconds (HOF + Callback).
-//  function abc (fn)
-//  {
-//     setTimeout(fn , 3000)
+ function abc (fn)
+ {
+    setTimeout(fn , 3000)
 
-//  }
+ }
 
-//  abc(function()
-// {
-//     console.log("calling after 3 seconds");
+ abc(function()
+{
+    console.log("calling after 3 seconds");
 
-// })
+})
 
 // ------------------------------------------------------------------------------------------------------------------------
 
 // Implement your own version of `.map()` as a higher-order function.
 
-// let arr = [1,2,3,4,5,6]
-// let ans =   arr.map((item)=>{
-//     return item*2;
-//   })
+let arr = [1,2,3,4,5,6]
+let ans =   arr.map((item)=>{
+    return item*2;
+  })
 
-// let arr = [1,2,3,4,5]
-// function mapkicopy(arr, fn)
-// {
-//     let newarr = [];  // .map new array mein push karta hai
-//     for (let i = 0; i < arr.length; i++) {
-//         newarr.push(fn(arr[i]));
+let arr = [1,2,3,4,5]
+function mapkicopy(arr, fn)
+{
+    let newarr = [];  // .map new array mein push karta hai
+    for (let i = 0; i < arr.length; i++) {
+        newarr.push(fn(arr[i]));
 
-//     }
-//     return newarr;
+    }
+    return newarr;
 
-// }
+}
 
-// let ans = mapkicopy(arr , function(val)
-// {
-//     return val*2;
-// })
+let ans = mapkicopy(arr , function(val)
+{
+    return val*2;
+})
 
 // -------------------------------------------------------------------------------------------------------------------------------
 
 // Write a function that uses closures to create a counter.
 
-//   function abc()
-//   {
-//     let count = 0;
-//     return function()
-//     {
-//        count++;
-//        console.log(count);
+  function abc()
+  {
+    let count = 0;
+    return function()
+    {
+       count++;
+       console.log(count);
 
-//     }
-//   }
-// let counter =  abc();
-// counter();
-// counter();
-// counter();
+    }
+  }
+let counter =  abc();
+counter();
+counter();
+counter();
 
 //-----------------------------------------------------------------------------------------------------------------------------------
 
