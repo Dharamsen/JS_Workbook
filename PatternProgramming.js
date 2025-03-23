5c303eb0bef5de8867aee2fbe3d259a876328a45
Dharamsen <dharamsen1612@gmail.com>
2025-03-23 12:26:59 +0530

    pattern problem

--- PatternProgramming.js
+++ PatternProgramming.js
@@ -1,132 +1,133 @@
 
-// // process.stdout.write is a function from nodes
-// * * * * * 
-// * * * * *
-// * * * * *
-// * * * * *
-// * * * * *
+process.stdout.write is a function from nodes
+* * * * * 
+* * * * *
+* * * * *
+* * * * *
+* * * * *
 
-// for (let i = 1; i <= 5; i++) {
-//     for (let j = 1; j <= 5; j++) {
-//         process.stdout.write("* ")               
+for (let i = 1; i <= 5; i++) {
+    for (let j = 1; j <= 5; j++) {
+        process.stdout.write("* ")               
         
-//     }
-//     console.log();
+    }
+    console.log();
     
     
-// }
+}
 
 // -------------------------------------------------------------------------------------------------------------------------------------
 
-// installing prompt-sync to get value from users
-// const prompt = require('prompt-sync')()
-// let n = prompt("enter the num");
-// for (let i = 1; i <= n; i++) {
-//     for (let j = 1; j <= n; j++) {
-//        process.stdout.write("* ")
+installing prompt-sync to get value from users
+
+const prompt = require('prompt-sync')()
+let n = prompt("enter the num");
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+       process.stdout.write("* ")
         
-//     }
-//     console.log();
+    }
+    console.log();
     
-// }
+}
 
 // ------------------------------------------------------------------------------------------------------------------------------------
 
-// right angle traingle 
+right angle traingle 
 
-// const prompt = require('prompt-sync')();
-// let n = prompt("enter a number ");
-// for (let i = 1; i <= n; i++) {
-//     for (let j = 1; j <= i; j++) {
-//         process.stdout.write("* ")
+const prompt = require('prompt-sync')();
+let n = prompt("enter a number ");
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+        process.stdout.write("* ")
         
-//     }
-//     console.log();
+    }
+    console.log();
     
     
-// }
+}
 
 // -----------------------------------------------------------------------------------------------------------------------------------
 
-// printing the same but in this case 
-// A
-// A B
-// A B C 
-// A B C D
+printing the same but in this case 
+A
+A B
+A B C 
+A B C D
 
-// const prompt = require('prompt-sync')();
-// let n = prompt("enter a number ");
-// for (let i = 1; i <= n; i++) {
-//    for (let j = 1; j <= i; j++) {
-//     process.stdout.write(String.fromCharCode(64+j)+ (" "));
+const prompt = require('prompt-sync')();
+let n = prompt("enter a number ");
+for (let i = 1; i <= n; i++) {
+   for (let j = 1; j <= i; j++) {
+    process.stdout.write(String.fromCharCode(64+j)+ (" "));
     
-//    }
-//   console.log("");
+   }
+  console.log("");
       
-// }
+}
 
 // ----------------------------------------------------------------------------------------------------------------------------------------
 
-// printing inverted traingle 
+printing inverted traingle 
 
-// const prompt = require('prompt-sync')();
-// let n = prompt("enter the num ")
-// for (let i = n; i >= 1; i--) {
-//   for (let j = 1; j <= i; j++) {
-//     process.stdout.write("* ")
+const prompt = require('prompt-sync')();
+let n = prompt("enter the num ")
+for (let i = n; i >= 1; i--) {
+  for (let j = 1; j <= i; j++) {
+    process.stdout.write("* ")
     
-//   }
-//   console.log("");
+  }
+  console.log("");
   
     
-// }
+}
 
 // --------------------------------------------------------------------------------------------------------------------------------------------
 
-//      *
-//     **
-//    ***
-//  *****
+     *
+    **
+   ***
+ *****
 
-// const prompt = require('prompt-sync')();
-// const n = prompt("enter a number");
-// for (let i = 1; i <= n; i++) {
-//     for (let j = 1; j <= n- i ; j++) {       
-//         process.stdout.write("-");
+const prompt = require('prompt-sync')();
+const n = prompt("enter a number");
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n- i ; j++) {       
+        process.stdout.write("-");
                
-//     }
-//     for (let j = 1; j <= i; j++) {
-//         process.stdout.write("* ");
+    }
+    for (let j = 1; j <= i; j++) {
+        process.stdout.write("* ");
 
-//     }
-//     console.log();
+    }
+    console.log();
     
          
-// }
+}
 
 // ---------------------------------------------------------------------------------------------------------------------------
 
-// creating X pattern 
+creating X pattern 
 
-// const prompt = require('prompt-sync')();
-// const n = Number( prompt("enter a number"));
-// for (let i = 1; i <= n; i++) {
-//     for (let j = 1; j <=n; j++) {
-//        if (i==j || i+j == n+1)
-//         {
-//             process.stdout.write("* ") 
-//         } 
-//         else process.stdout.write("  ")            
+const prompt = require('prompt-sync')();
+const n = Number( prompt("enter a number"));
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <=n; j++) {
+       if (i==j || i+j == n+1)
+        {
+            process.stdout.write("* ") 
+        } 
+        else process.stdout.write("  ")            
        
-//     }
-//     console.log();
+    }
+    console.log();
     
     
-// }
+}
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------
 
-// creating a V pattern 
+creating a V pattern 
 
 const prompt = require('prompt-sync')();
 const n = Number(prompt("enter the num"));
